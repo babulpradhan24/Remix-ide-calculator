1002d62c89da962f089a12ceb92a5be4ecf0dfec
babulpradhan24 <babulupradhan4011@gmail.com>
2025-01-10 01:19:57 +0530

    Add files via upload
    its a calculator made by using solidity .

---
+++ calculator.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.0;
+
+
+contract    Calculator{ 
+    uint256 result =0;
+
+    function add(uint256 num) public{
+        result += num;
+    }
+
+    function substract(uint256 num) public{
+        result -= num;
+    }
+    function multiply(uint256 num) public{
+        result *= num;
+    } 
+    function divide(uint256 num) public{
+        result /= num;
+    }
+    function get() public view returns (uint256){
+        return result;
+    }
+}
